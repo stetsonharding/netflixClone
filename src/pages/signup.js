80b1cd6d16b1a9a867e4f2ed785b070ae66157e2
Stetson Harding <stetson2001@msn.com>
2021-04-29 08:04:05 -0700

    signup page completed

---
+++ src/pages/signup.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
+import { FooterContainer } from "../containers/footer";
+import * as ROUTES from "../constants/routes";
+
+export default function SignUp() {
+  const [firstName, setFirstName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = firstName === "" || emailAddress === "" || password === "";
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignup} method="POST">
+            <Form.Input
+              type="text"
+              placeholder="First Name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              type="email"
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              value={password}
+              autoComplete="off"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign Up
+            </Form.Submit>
+            <Form.Text>
+              Already a user? <Form.Link to="/signin">Sign in here.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
+}

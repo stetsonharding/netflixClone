c784f62a35c5a71d49609435080721afee124e1b
Stetson Harding <stetson2001@msn.com>
2021-05-03 21:31:50 -0700

    features added to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,6 +8,9 @@ import {
   Group,
   Text,
   Link,
+  Feature,
+  FeatureCallOut,
+  PlayButton,
 } from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
@@ -40,4 +43,19 @@ Header.Link = function HeaderLink({ children, ...restProps }) {
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
   return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restProps
+}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+  return <PlayButton {...restProps}>{children}</PlayButton>;
 };
